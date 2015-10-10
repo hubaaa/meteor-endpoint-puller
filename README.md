1ee6383182b8860698bd14ecc83a289fa52b64b9
Ronen Babayoff <ronen@lavaina.com>
2015-10-10 01:03:16 -0400

    Rename package and update readme

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# meteor-endpoint-puller
+# hubaaa:endpoint-puller
+
+Pulls API endpoints, checking for new data and passing it through json pipes.
+
+Supports the following HTTP request / response headers to avoid non-necessary pulling and API rate limit penalties:
+
+- Last-Modified
+
+- If-Modified-Since
+
+# API
+
+`puller = new EndpointPuller(options == {})`
+
+## options
+
+###  endpoint
+
+String. The absolute URL to the endpoint.
+
+### httpOptions
+
+Json. Passed as is to meteor's HTTP.get
+
+### pullOptions
+
+Json. Pull specific options, including:
+
+- defaultPullInterval: in milliseconds, default 5 seconds. Can't be less than 1 second.
+
+### jsonPipeOptions
+
+Since an EndpointPuller inherits from JsonPipe, all the option supported by [JsonPipe](https://atmospherejs.com/hubaaa/json-pipes).
+
+Passed up to the JsonPipe base class.
+
+# Coming Soon
+
+- Support either providing an already existing jsonPipe or an options doc to create a json pipe.
--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'endpoint-puller',
-  version: '0.0.1',
+  name: 'hubaaa:endpoint-puller',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Pulls API endpoints, checking for new data and passing it through json pipes.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/hubaaa/meteor-endpoint-puller',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -19,8 +19,8 @@ Package.onUse(function(api) {
     'http',
     'practicalmeteor:loglevel@1.2.0_2',
     'practicalmeteor:chai@2.1.0_1',
-    'easy-meteor-settings',
-    'json-pipes'
+    'hubaaa:easy-meteor-settings',
+    'hubaaa:json-pipes@0.1.1'
   ], 'server');
 
   api.addFiles('EndpointPuller.coffee', 'server');
@@ -33,7 +33,7 @@ Package.onTest(function(api) {
     'practicalmeteor:mocha@2.1.0_3'
   ], 'server');
 
-  api.use('endpoint-puller', 'server');
+  api.use('hubaaa:endpoint-puller@0.1.0', 'server');
 
   api.addFiles('EndpointPullerTest.coffee', 'server');
 });
